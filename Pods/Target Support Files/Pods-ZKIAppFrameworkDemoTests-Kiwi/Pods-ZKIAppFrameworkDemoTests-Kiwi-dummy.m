d1e5ca95ccb957c71ead2858510a378e39144171
thewingofthesky <thewingofthesky@gmail.com>
2015-06-04 16:08:07 +0800

    创建工程

---
+++ Pods/Target Support Files/Pods-ZKIAppFrameworkDemoTests-Kiwi/Pods-ZKIAppFrameworkDemoTests-Kiwi-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ZKIAppFrameworkDemoTests_Kiwi : NSObject
+@end
+@implementation PodsDummy_Pods_ZKIAppFrameworkDemoTests_Kiwi
+@end
---
+++ ZKIAppFrameworkDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZKIAppFrameworkDemo
+//
+//  Created by Dai Qinfu on 15/6/4.
+//  Copyright (c) 2015年 Wingzki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
