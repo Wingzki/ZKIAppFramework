b5d5bc1688b925d607d9c948aec7a133f7886d68
thewingofthesky <thewingofthesky@gmail.com>
2015-07-01 11:26:49 +0800

    添加UIViewController+RequestStatusView

---
+++ ZKIAppFrameworkDemo/UIViewController+RequestStatusView/UIViewController+RequestStatusView.h
@@ -0,0 +1,13 @@
+//
+//  UIViewController+RequestStatusView.h
+//  ZKIAppFrameworkDemo
+//
+//  Created by Dai Qinfu on 15/7/1.
+//  Copyright (c) 2015年 Wingzki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (RequestStatusView)
+
+@end
---
+++ ZKIAppFrameworkDemo/UIViewController+RequestStatusView/UIViewController+RequestStatusView.m
@@ -0,0 +1,13 @@
+//
+//  UIViewController+RequestStatusView.m
+//  ZKIAppFrameworkDemo
+//
+//  Created by Dai Qinfu on 15/7/1.
+//  Copyright (c) 2015年 Wingzki. All rights reserved.
+//
+
+#import "UIViewController+RequestStatusView.h"
+
+@implementation UIViewController (RequestStatusView)
+
+@end
